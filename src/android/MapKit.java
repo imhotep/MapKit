a6e8092fabce69e9ddbe1b5ec0cc40fb6b09a99f
Anis Kadri <anis@apache.org>
2013-04-09 16:22:31 -0700

    updating MapKit features

--- src/android/MapKit.java
+++ src/android/MapKit.java
@@ -7,15 +7,19 @@ import org.apache.cordova.api.CordovaPlugin;
 import org.apache.cordova.api.LOG;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapKit extends CordovaPlugin {
 
@@ -28,19 +32,29 @@ public class MapKit extends CordovaPlugin {
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
-		main = new RelativeLayout(cordova
-				.getActivity());
-		mapView = new MapView(cordova.getActivity(), new GoogleMapOptions());
+		main = new RelativeLayout(cordova.getActivity());
 	}
 
-	public void showMap() {
+	public void showMap(final JSONObject options) {
 		try {
 			cordova.getActivity().runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
+					double latitude = 0, longitude = 0;
+					int height = 460;
+					boolean atBottom = false;
+					try {
+						height = options.getInt("height");
+						latitude = options.getDouble("lat");
+						longitude = options.getDouble("lon");
+						atBottom = options.getBoolean("atBottom");
+					} catch (JSONException e) {
+						LOG.e(TAG, "Error reading options");
+					}
+					mapView = new MapView(cordova.getActivity(),
+							new GoogleMapOptions());
 					root = (ViewGroup) webView.getParent();
 					root.removeView(webView);
-					
 					main.addView(webView);
 
 					cordova.getActivity().setContentView(main);
@@ -50,17 +64,29 @@ public class MapKit extends CordovaPlugin {
 					} catch (GooglePlayServicesNotAvailableException e) {
 						e.printStackTrace();
 					}
+
 					RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-							LayoutParams.MATCH_PARENT, 400);
-					params.addRule(RelativeLayout.ALIGN_PARENT_TOP,
-							RelativeLayout.TRUE);
+							LayoutParams.MATCH_PARENT, height);
+					if (atBottom) {
+						params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,
+								RelativeLayout.TRUE);
+					} else {
+						params.addRule(RelativeLayout.ALIGN_PARENT_TOP,
+								RelativeLayout.TRUE);
+					}
 					params.addRule(RelativeLayout.CENTER_HORIZONTAL,
 							RelativeLayout.TRUE);
 
 					mapView.setLayoutParams(params);
 					mapView.onCreate(null);
-					mapView.onResume(); // FIXME: I wish there was a better way than this...
+					mapView.onResume(); // FIXME: I wish there was a better way
+										// than this...
 					main.addView(mapView);
+
+					// Moving the map to lot, lon
+					mapView.getMap().moveCamera(
+							CameraUpdateFactory.newLatLngZoom(new LatLng(
+									latitude, longitude), 15));
 					cCtx.success();
 				}
 			});
@@ -69,19 +95,21 @@ public class MapKit extends CordovaPlugin {
 			cCtx.error("MapKitPlugin::showMap(): An exception occured");
 		}
 	}
-	
+
 	private void hideMap() {
 		try {
 			cordova.getActivity().runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					mapView.onDestroy();
-					main.removeView(webView);
-					main.removeView(mapView);
-					root.addView(webView);
-					cordova.getActivity().setContentView(root);
-					cCtx.success();
-
+					if (mapView != null) {
+						mapView.onDestroy();
+						main.removeView(webView);
+						main.removeView(mapView);
+						root.addView(webView);
+						cordova.getActivity().setContentView(root);
+						mapView = null;
+						cCtx.success();
+					}
 				}
 			});
 		} catch (Exception e) {
@@ -90,19 +118,68 @@ public class MapKit extends CordovaPlugin {
 		}
 	}
 
+	public void addMapPins(final JSONArray pins) {
+		try {
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if (mapView != null) {
+						try {
+							for (int i = 0, j = pins.length(); i < j; i++) {
+								JSONArray coordinates = pins.getJSONArray(i);
+								double latitude = coordinates.getDouble(0);
+								double longitude = coordinates.getDouble(1);
+
+								// adding Marker
+								// TODO add ability to set info bubble with title()
+								mapView.getMap().addMarker(
+										new MarkerOptions()
+												.position(new LatLng(latitude,
+														longitude)));
+							}
+							cCtx.success();
+						} catch (JSONException e) {
+							e.printStackTrace();
+							LOG.e(TAG, "An error occurred while reading pins");
+							cCtx.error("An error occurred while reading pins");
+						}
+					}
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+			cCtx.error("MapKitPlugin::addMapPins(): An exception occured");
+		}
+	}
+
+	public void clearMapPins() {
+		try {
+			cordova.getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if (mapView != null) {
+						mapView.getMap().clear();
+						cCtx.success();
+					}
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+			cCtx.error("MapKitPlugin::clearMapPins(): An exception occured");
+		}
+	}
+
 	public boolean execute(String action, JSONArray args,
 			CallbackContext callbackContext) throws JSONException {
 		cCtx = callbackContext;
-		if(action.compareTo("showMap") == 0) {
-			showMap();
-		} else if(action.compareTo("hideMap") == 0) {
+		if (action.compareTo("showMap") == 0) {
+			showMap(args.getJSONObject(0));
+		} else if (action.compareTo("hideMap") == 0) {
 			hideMap();
-		} else if(action.compareTo("setMapData") == 0) {
-			// TODO
-		} else if(action.compareTo("addMapPins") == 0) {
-			// TODO
-		} else if(action.compareTo("clearMapPins") == 0) {
-			
+		} else if (action.compareTo("addMapPins") == 0) {
+			addMapPins(args.getJSONArray(0));
+		} else if (action.compareTo("clearMapPins") == 0) {
+			clearMapPins();
 		}
 		LOG.d(TAG, action);
 
@@ -112,21 +189,27 @@ public class MapKit extends CordovaPlugin {
 	@Override
 	public void onPause(boolean multitasking) {
 		LOG.d(TAG, "MapKitPlugin::onPause()");
+		if (mapView != null) {
+			mapView.onPause();
+		}
 		super.onPause(multitasking);
-		mapView.onPause();
 	}
 
 	@Override
 	public void onResume(boolean multitasking) {
 		LOG.d(TAG, "MapKitPlugin::onResume()");
+		if (mapView != null) {
+			mapView.onResume();
+		}
 		super.onResume(multitasking);
-		mapView.onResume();
 	}
 
 	@Override
 	public void onDestroy() {
 		LOG.d(TAG, "MapKitPlugin::onDestroy()");
+		if (mapView != null) {
+			mapView.onDestroy();
+		}
 		super.onDestroy();
-		mapView.onDestroy();
 	}
 }
--- www/MapKit.js
+++ www/MapKit.js
@@ -1,15 +1,6 @@
 
 (function() {
 
-var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
-
-	/*
-	* PhoneGap is available under *either* the terms of the modified BSD license *or* the
-	* MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
-	*
-	* Copyright (c) 2005-2010, Nitobi Software Inc., Brett Rudd, Jesse MacFadyen
-	*/
-
 	var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
 
 	var MapKit = function() {
@@ -25,40 +16,19 @@ var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to 
 	MapKit.prototype = {
 
 		showMap: function(success, error) {
-			cordovaRef.exec(success, error, 'MapKitView', 'showMap', []);
+			cordovaRef.exec(success, error, 'MapKit', 'showMap', [this.options]);
 		},
 
-		setMapData: function(success, error, options) {
-			/*
-			buttonCallback: String, string callback function
-			height: Number, - pixels
-			diameter: Number, - meters
-			atBottom: Bool,
-			lat: Number,
-			lon: Number
-			*/
-			for (var v in options) {
-				if (options.hasOwnProperty(v)) {
-					this.options[v] = options[v];
-				}
-			}
-			cordovaRef.exec(success, error, 'MapKitView', 'setMapData', [this.options]);
-		},
-
-		addMapPins: function(pins) {
-			var pinStr = '[]';
-			if (pins) {
-				pinStr = JSON.stringify(pins);
-			}
-			cordovaRef.exec(success, error, 'MapKitView', 'addMapPins', [pinStr]);
+		addMapPins: function(pins, success, error) {
+			cordovaRef.exec(success, error, 'MapKit', 'addMapPins', [pins]);
 		},
 
 		clearMapPins: function(success, error) {
-			cordovaRef.exec(success, error, 'MapKitView', 'clearMapPins', []);
+			cordovaRef.exec(success, error, 'MapKit', 'clearMapPins', []);
 		},
 
-		hideMap: function() {
-			cordovaRef.exec(success, error, 'MapKitView', 'hideMap', []);
+		hideMap: function(success, error) {
+			cordovaRef.exec(success, error, 'MapKit', 'hideMap', []);
 		}
 
 	};
