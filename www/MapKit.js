2f1d31646d0562e14065f1661be8902b60f4b910
Jason Rosenbaum <seminal@theseminal.com>
2014-10-05 15:14:43 -0400

    optionally pass in options as arguments on ShowMap

--- www/MapKit.js
+++ www/MapKit.js
@@ -1,70 +1,87 @@
-var exec = require('cordova/exec');
 
-var MapKit = function() {
-	this.mapType = {
-		MAP_TYPE_NONE: 0, //No base map tiles.
-		MAP_TYPE_NORMAL: 1, //Basic maps.
-		MAP_TYPE_SATELLITE: 2, //Satellite maps with no labels.
-		MAP_TYPE_TERRAIN: 3, //Terrain maps.
-		MAP_TYPE_HYBRID: 4 //Satellite maps with a transparent layer of major streets.
+(function() {
+
+	var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
+
+	var MapKit = function() {
+		this.options = {
+			height: 460,
+			diameter: 1000,
+			atBottom: true,
+			lat: 49.281468,
+			lon: -123.104446
+		};
+
+		this.mapType = {
+            MAP_TYPE_NONE: 0, //No base map tiles.
+            MAP_TYPE_NORMAL: 1, //Basic maps.
+            MAP_TYPE_SATELLITE: 2, //Satellite maps with no labels.
+            MAP_TYPE_TERRAIN: 3, //Terrain maps.
+            MAP_TYPE_HYBRID: 4 //Satellite maps with a transparent layer of major streets.
+        };
+
+        this.iconColors = {
+            HUE_RED: 0.0,
+            HUE_ORANGE: 30.0,
+            HUE_YELLOW: 60.0,
+            HUE_GREEN: 120.0,
+            HUE_CYAN: 180.0,
+            HUE_AZURE: 210.0,
+            HUE_BLUE: 240.0,
+            HUE_VIOLET: 270.0,
+            HUE_MAGENTA: 300.0,
+            HUE_ROSE: 330.0
+        };
 	};
 
-	this.iconColors = {
-		HUE_RED: 0.0,
-		HUE_ORANGE: 30.0,
-		HUE_YELLOW: 60.0,
-		HUE_GREEN: 120.0,
-		HUE_CYAN: 180.0,
-		HUE_AZURE: 210.0,
-		HUE_BLUE: 240.0,
-		HUE_VIOLET: 270.0,
-		HUE_MAGENTA: 300.0,
-		HUE_ROSE: 330.0
+	MapKit.prototype = {
+
+		showMap: function(success, error, options) {
+			if (options) {
+				cordovaRef.exec(success, error, 'MapKit', 'showMap', [options]);
+			} else {
+				cordovaRef.exec(success, error, 'MapKit', 'showMap', [this.options]);
+			}
+			
+		},
+
+		addMapPins: function(pins, success, error) {
+			cordovaRef.exec(success, error, 'MapKit', 'addMapPins', [pins]);
+		},
+
+		clearMapPins: function(success, error) {
+			cordovaRef.exec(success, error, 'MapKit', 'clearMapPins', []);
+		},
+
+		hideMap: function(success, error) {
+			cordovaRef.exec(success, error, 'MapKit', 'hideMap', []);
+		},
+
+		changeMapType: function(mapType, success, error) {
+			cordovaRef.exec(success, error, 'MapKit', 'changeMapType', [mapType ? { "mapType": mapType } :{ "mapType": 0 }]);
+		},
+		
+		setMapData: function(options, success, error) {
+			/*
+			buttonCallback: String, string callback function
+			height: Number, - pixels
+			diameter: Number, - meters
+			atBottom: Bool,
+			lat: Number,
+			lon: Number
+			*/
+			for (var v in options) {
+				if (options.hasOwnProperty(v)) {
+					this.options[v] = options[v];
+				}
+			}
+			cordovaRef.exec(success, error, 'MapKit', 'setMapData', this.options);
+		}
+
 	};
-};
 
-// Returns default params, overrides if provided with values
-function setDefaults(options) {
-    var defaults = {
-        height: 460,
-		diameter: 1000,
-		atBottom: true,
-		lat: 49.281468,
-		lon: -123.104446
-    };
+	cordovaRef.addConstructor(function() {
+		window.mapKit = new MapKit();
+	});
 
-    if (options) {
-        for(var i in defaults) 
-            if(typeof options[i] === "undefined") 
-                options[i] = defaults[i];
-    }
-
-    return options;
-}
-
-MapKit.prototype = {
-
-	showMap: function(success, error, options) {
-	    options = setDefaults(options);
-		exec(success, error, 'MapKit', 'showMap', [options]);
-	},
-
-	addMapPins: function(pins, success, error) {
-		exec(success, error, 'MapKit', 'addMapPins', [pins]);
-	},
-
-	clearMapPins: function(success, error) {
-		exec(success, error, 'MapKit', 'clearMapPins', []);
-	},
-
-	hideMap: function(success, error) {
-		exec(success, error, 'MapKit', 'hideMap', []);
-	},
-
-	changeMapType: function(mapType, success, error) {
-		exec(success, error, 'MapKit', 'changeMapType', [mapType ? { "mapType": mapType } :{ "mapType": 0 }]);
-	}
-
-};
-
-module.exports = new MapKit();
+})();
